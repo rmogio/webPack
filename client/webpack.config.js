479411ca595b907103083f184fc1310f0bcde21c
Ricardo Mogio <ricardo.mogio@gmail.com>
2020-05-21 19:20:27 -0300

    Minificado arquivos css, html, instalado novas dependencias

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,18 +2,39 @@ const path = require('path')
 const babiliPlugin = require('babili-webpack-plugin')
 const extractTextPlugin = require('extract-text-webpack-plugin')
 const optmizeCSSAssetsPlugins = require('optimize-css-assets-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const webpack = require('webpack')
 
 let plugins= []
 
+plugins.push(new HtmlWebpackPlugin({
+	hash: true,
+	minify: {
+		html5:true,
+		collapseWhitespace: true,
+		removeComments: true
+	},
+	filename: 'index.html',
+	template: __dirname+'/main.html'
+}))
+
 plugins.push(new extractTextPlugin('styles.css'))
+
 plugins.push(new webpack.ProvidePlugin({
     '$':'jquery/dist/jquery.js',
     'jQuery': 'jquery/dist/jquery.js'
 }))
 
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+	name:'vendor',
+	filename:'vendor.bundle.js'
+}))
+
 if(process.env.NODE_ENV == 'production'){
+	
+	plugins.push(new webpack.optimize.ModuleConcatenationPlugin())
+
   plugins.push(new babiliPlugin())
 
   plugins.push(new optmizeCSSAssetsPlugins({
@@ -28,11 +49,13 @@ if(process.env.NODE_ENV == 'production'){
 }
 
 module.exports = {
-  entry: './app-src/app.js',
+  entry: {
+		app: './app-src/app.js',
+		vendor: ['jquery','bootstrap','reflect-metadata']
+	},
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist'
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
